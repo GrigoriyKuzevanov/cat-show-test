c3835c3103ced7751795d50fdead61168341e7ca
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-03 17:57:38 +0500

    add routes and crud for get kitten by id, get kittens by breed.

--- src/app/crud/crud_kittens.py
+++ src/app/crud/crud_kittens.py
@@ -5,8 +5,19 @@ from app.models import Kitten
 from app.schemas import schemas_kittens
 
 
-def read_all_kittens(session: Session, limit: int, skip: int):
+def read_all_kittens(session: Session, limit: int, skip: int) -> list:
     stmt = select(Kitten).limit(limit).offset(skip)
     db_kittens = session.scalars(stmt).all()
 
     return db_kittens
+
+
+def read_kittens_by_breed_id(session: Session, breed_id: int) -> list:
+    stmt = select(Kitten).where(Kitten.breed_id == breed_id)
+    db_kittens = session.scalars(stmt).all()
+
+    return db_kittens
+
+
+def read_kitten_by_id(session: Session, kitten_id: int) -> Kitten | None:
+    return session.get(Kitten, kitten_id)
--- src/app/routes/kittens.py
+++ src/app/routes/kittens.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
@@ -11,3 +11,23 @@ kittens_router = APIRouter(prefix="/kittens", tags=["kittens"])
 @kittens_router.get("/", response_model=list[schemas_kittens.KittenOut])
 def get_kittens(limit: int = 10, skip: int = 0, session: Session = Depends(get_db)):
     return crud_kittens.read_all_kittens(session=session, limit=limit, skip=skip)
+
+
+@kittens_router.get(
+    "/by_breed/{breed_id}", response_model=list[schemas_kittens.KittenOut]
+)
+def get_kittens_by_breed(breed_id: int, session: Session = Depends(get_db)):
+    return crud_kittens.read_kittens_by_breed_id(session=session, breed_id=breed_id)
+
+
+@kittens_router.get("/{kitten_id}", response_model=schemas_kittens.KittenOut)
+def get_kitten(kitten_id: int, session: Session = Depends(get_db)):
+    db_kitten = crud_kittens.read_kitten_by_id(session=session, kitten_id=kitten_id)
+
+    if not db_kitten:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Kitten with id: {kitten_id} does not exist",
+        )
+
+    return db_kitten
